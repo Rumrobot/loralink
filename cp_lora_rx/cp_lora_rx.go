aa2ce96d656fce03f916ebcc70a4ebe44c5cc4e6
Rumrobot <rumrobot@gmail.com>
2024-07-02 08:09:54 +0200

    New post request test

--- cp_lora_rx/cp_lora_rx.go
+++ cp_lora_rx/cp_lora_rx.go
@@ -4,22 +4,17 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	// "os/signal"
 	"time"
-//	"bufio"
-//	"net"
-//    "os"	
-	"bytes"
-	"encoding/json"
+	// "os/signal"
+	//	"bufio"
+	//	"net"
+	//    "os"
+	// "bytes"
 	"net/http"
-        
+
 	"lora"
 )
 
-type RequestData struct {
-	auth string `json:"auth"`
-	data  string `json:"json:"data"`
-}
 const (
 	CodingRate4_5 = 0x01 //  7     0     LoRa coding rate: 4/5
 	CodingRate4_6 = 0x02 //  7     0                       4/6
@@ -52,35 +47,24 @@ const (
 func main() {
 	var lora lora.LORA
 	fmt.Println("Starting LORA gateway")
-	postData := RequestData{
-   		auth: "k2j39s92k!",
-	}
-
-	//defer res.Body.Close()
+	//	auth: "k2j39s92k!",
 
 	//c, err := net.Dial("tcp", "anyvej11.dk:8087")
-    //if err != nil {
+	//if err != nil {
 	// 		fmt.Println(err)
 	// 		return
 	// }
 	// fmt.Fprintf(c, "Hello from LORAgateway\n")
-	
+
 	err := lora.Open("/dev/spidev0.0")
 	if err != nil {
 		log.Fatal(err)
-	}
-	
-	 
-	client := &http.Client{}
-	// client.Do(r)
-	if err != nil {
 		panic(err)
 	}
 
-
-	// Set lora mode + standby 
-	fmt.Println("Setting mode LORA + standby" )
-    lora.SetLORAmode(0x0)
+	// Set lora mode + standby
+	fmt.Println("Setting mode LORA + standby")
+	lora.SetLORAmode(0x0)
 	lora.SetLORAmode(0x1)
 	fmt.Println("Setting mode LORA + standby [done]")
 
@@ -89,65 +73,56 @@ func main() {
 
 	lora.SetLORAmode(0x0)
 	lora.SetLORAmode(0x1)
-	
+
 	lora.SetFrequency(436000000)
- 	lora.SetBanddwidth(Bandwidth_31_25)
+	lora.SetBanddwidth(Bandwidth_31_25)
 	lora.SetSpreadingFactor(SpreadingFactor10)
 	lora.SetCodingRate(CodingRate4_5)
 	lora.SetSyncWord(0x12)
-	
+
 	lora.SetLORAmode(0x5)
-	var data string 
-	
-	var debug int = 0	
-    for (true ) {	
-		fmt.Print(".")
-		irq , _ := lora.Read(0x12) 
-		if irq & 0x50 == 0x50 {
-		  	if (debug == 1) {
-				lora.Read(0x0d)
-				lora.Read(0x10)
-				lora.Read(0x13)
-				lora.Read(0x15)
-				lora.Read(0x18)
-		    }
-			lora.Write(0x12, 0x50)
-			lora.Write(0x12, 0x50)
-			data = lora.Getpacket()
-		    fmt.Printf("Data: %s\n", data)
-			// split on space 
-			data_arr := strings.Split(data, " ")
-			if len(data_arr) == 3 {	
-				fmt.Printf("Data: %s\n", data_arr[2]) 
-				
-				
-				// Insert the variable into the data field
-				postData.data = data_arr[2]
-			
-				//jsonData, err := json.Marshal(postData)
-				if err != nil {
-					fmt.Println("Error marshalling JSON data:", err)
-					return
-				}
-			
-				posturl := "https://rumrobot.dk/temp" // Replace with the actual URL
-			
-				req, err := http.NewRequest("POST", posturl, bytes.NewBuffer(postData))
-				if err != nil {
-					fmt.Println("Error creating HTTP request:", err)
-					return
-				}
-			
-				req.Header.Add("Content-Type", "application/json")
-				fmt.Printf("%s ", postData)
-				client.Do(req)
-				if err != nil {
-				panic(err)
-				
-				}
-			}
+
+	for true {
+		var data = getTemperature()
+
+		var request = strings.NewReader(`
+		{
+			"auth": "k2j39s92k!",
+			"data": "1234"
 		}
-	
-		time.Sleep(8 * time.Second) 
-	} 
+		`)
+
+		if data != "error" {
+			http.Post("https://rumrobot.dk/temp", "application/json", request)
+		}
+
+		time.Sleep(8 * time.Second)
+	}
+}
+
+func getTemperature() string {
+	var debug int = 0
+
+	fmt.Print(".")
+	irq, _ := lora.Read(0x12)
+	if irq&0x50 == 0x50 {
+		if debug == 1 {
+			lora.Read(0x0d)
+			lora.Read(0x10)
+			lora.Read(0x13)
+			lora.Read(0x15)
+			lora.Read(0x18)
+		}
+		lora.Write(0x12, 0x50)
+		lora.Write(0x12, 0x50)
+		var data = lora.Getpacket()
+
+		// Parse data
+		data_arr := strings.Split(data, " ")
+		if len(data_arr) == 3 {
+			fmt.Printf("Data: %s\n", data_arr[2])
+			return data_arr[2]
+		}
+	}
+	return "error"
 }
--- cp_lora_rx/go.mod
+++ cp_lora_rx/go.mod
@@ -1,3 +1,4 @@
 module github.com/stianeikeland/go-rpio
+module lora
 
 go 1.21.8
